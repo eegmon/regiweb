62fad17dfec00c7dc1b7829cd8ecda10b3c01fa8
eegmon <74459174+eegmon@users.noreply.github.com>
2026-05-23 20:51:37 +0900

    데이터베이스 변경
    개발환경과 호스팅 환경에서의 데이터베이스를 분리하였습니다.

--- web/settings.py
+++ web/settings.py
@@ -77,21 +77,14 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "regiweb-database",
-#         "USER": "regiuser",
-#         "PASSWORD": "register00",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "regiweb-database",
+        "USER": "regiuser",
+        "PASSWORD": "register00",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
