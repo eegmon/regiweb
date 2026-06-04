30c6d10787e7afdf13abf7597ef4f954d2f8922d
차산 <wlskrkejs9191@gmail.com>
2026-06-04 22:46:37 +0900

    브랜치옮김 ^^;;
    제가 잘못된 브랜치에서 작업을 하고 있었다네요
    정말 충격이네요
    다른 브랜치에서 작업하던거 끌고오는 과정에서 파일이 잘못 합쳐졌는지 문법오류가 났었는데 것도 해결했슴두

--- register/urls.py
+++ register/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'), # 주소는 /accounts/signup/ 이 됩니다.
+    path('login/', auth_views.LoginView.as_view(template_name='register/login.html'), name='login'),
 ]
