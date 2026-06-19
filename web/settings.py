1c0c04a1a62b530825c3097ef78e3e2fc486f2c4
차산 <wlskrkejs9191@gmail.com>
2026-06-19 16:10:32 +0900

    디버그

--- web/settings.py
+++ web/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&-n5u#q!qxeun&ijswu2ir(gqg_y=f)8o4pxu8_i0l+7h2#iis'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
